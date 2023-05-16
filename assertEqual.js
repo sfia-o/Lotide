140851b585c3dfef9245aaaf945205daefd7639a
sfia-o <sofia.vl.oliveira@gmail.com>
2023-05-16 23:47:35 +0000

    Created assert function

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(:"Assertion Passed: " + actual + " === "  + expected);
+  } else {
+    console.log("Assertion Failed: " + actual + " !==" + expected);
+  }
+
+};
+
+assertEqual("Boot", "Bootcamp");
+assertEqual(1, 7);
+
