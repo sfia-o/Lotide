3d1b93d1114fe4921b6199bcb97c45342f29f52a
sfia-o <sofia.vl.oliveira@gmail.com>
2023-05-17 22:16:05 +0000

    Creating a function that compares two equal arrays

---
+++ eqArrays.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual}  ===  ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2){
+
+  if (arr1.length !== arr2.length){
+    return false;
+  } else {
+      for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]){
+        return false;
+      }
+     } 
+     return true;
+  } 
+}  
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]))// => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
+
+console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
+console.log(assertEqual(eqArrays(["yes", "no"], ["yes", "no"]), false));
