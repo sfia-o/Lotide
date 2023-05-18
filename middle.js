c694d85bd131b00dcc6bd319c9b6c91a27c47e5a
sfia-o <sofia.vl.oliveira@gmail.com>
2023-05-18 23:31:47 +0000

    Fixed lint issues

--- middle.js
+++ middle.js
@@ -1,43 +1,43 @@
-const eqArrays = function(arr1, arr2){
+const eqArrays = function(arr1, arr2) {
 
-  if (arr1.length !== arr2.length){
+  if (arr1.length !== arr2.length) {
     return false;
   } else {
-      for (let i = 0; i < arr1.length; i++) {
-        if (arr1[i] !== arr2[i]){
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
         return false;
       }
-     } 
-     return true;
-  } 
-}  
+    }
+    return true;
+  }
+};
 
 const assertArraysEqual = function(arr3, arr4) {
-  if (eqArrays(arr3, arr4)){
+  if (eqArrays(arr3, arr4)) {
     console.log("Arrays are equal");
   } else {
     console.log("Arrays are not equal");
   }
-}
+};
 
 const middle = function(arr) {
 
   let mid = [];
   
   if (arr.length <= 2) {
-    return[];
-  } else if (arr.length % 2 == 1) {
-    mid.push(Math.floor(arr.length/2));
+    return [];
+  } else if (arr.length % 2 === 1) {
+    mid.push(Math.floor(arr.length / 2));
     return [arr[mid[0]]];
     
-  } else if (arr.length % 2 == 0) {
-    mid.push(Math.floor(arr.length/2) - 1);
+  } else if (arr.length % 2 === 0) {
+    mid.push(Math.floor(arr.length / 2) - 1);
     mid.push(Math.floor(arr.length / 2));
     return [arr[mid[0]], arr[mid[1]]];
     
-  } 
-
   }
+
+};
 
 
 console.log(middle([1, 2]));
