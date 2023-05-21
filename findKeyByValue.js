3f2a35011be17553b47c6276230d1d68e46e36d3
sfia-o <sofia.vl.oliveira@gmail.com>
2023-05-21 16:41:23 +0000

    Initial commit

---
+++ findKeyByValue.js
@@ -0,0 +1,32 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual}  ===  ${expected}`);
+  } else {
+    console.log(`🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+const findKeyByValue = function(object, value) {
+
+  // we're iterating through an object
+  for (const key in object) {
+  
+    // through this iteration we want to find the key that stores a string that is equal to our input value
+    if (object[key] === value) {
+      return key;
+    }
+  
+  }
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+console.log(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"));
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
