a781872477f002817c2c911e91d98a19de9c578a
sfia-o <sofia.vl.oliveira@gmail.com>
2023-06-06 22:39:47 +0000

    Refactoring and created test file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,24 +1,14 @@
-const eqArrays = function(arr1, arr2){
-
-  if (arr1.length !== arr2.length){
-    return false;
-  } else {
-      for (let i = 0; i < arr1.length; i++) {
-        if (arr1[i] !== arr2[i]){
-        return false;
-      }
-     } 
-     return true;
-  } 
-}  
+const eqArrays = require('./eqArrays')
 
 const assertArraysEqual = function(arr3, arr4) {
   if (eqArrays(arr3, arr4)){
-    console.log(`✅ Assertion Passed: ${arr3}  ===  ${arr4}`);
-  } else {
+    console.log(`✅ Assertion Passed: ${arr3}  ===  ${arr4}`); 
+  } 
+
+  if (!eqArrays(arr3,arr4)) {
     console.log(`🛑Assertion Failed: ${arr3} !== ${arr4}`);
   }
-}
+};
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [4, 5, 6]);
+module.exports = assertArraysEqual;
+
