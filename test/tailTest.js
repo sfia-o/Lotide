66f9072056e1b6662c2d0bf968736253459a400c
sfia-o <sofia.vl.oliveira@gmail.com>
2023-06-07 00:16:06 +0000

    Incorporated mocha and chai into test file

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,11 +1,30 @@
-const assertEqual = require('../assertEqual')
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-tail([5, 6, 8, 99]);
-tail(["hey", "hello", "and goodmorning!"]);
-tail([1]);
-tail([]);
+describe('#tail', () => {
+  it('returns [6, 8, 99] when passed [5, 6, 8, 99]', () => {
+    assert.deepEqual(tail([5, 6, 8, 99]), [6, 8, 99]);
+  });
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); 
-assertEqual(words.length, 3);
+  it('returns ["hello", "and goodmorning!"] when passed ["hey", "hello", "and goodmorning!]', () => {
+    assert.deepEqual(tail(['hey', 'hello', 'and goodmorning!']), ['hello', 'and goodmorning!']);
+  });
+
+  it('returns an empty array when passed an array with only one element', () => {
+    assert.deepEqual(tail(['hey']), []);
+  });
+
+  it('returns an empty array when passed an empty array', () => {
+    assert.deepEqual(tail([]), []);
+  });
+
+});
+
+// tail([5, 6, 8, 99]);
+// tail(["hey", "hello", "and goodmorning!"]);
+// tail([1]);
+// tail([]);
+
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); 
+// assertEqual(words.length, 3);
