1bb06f8c57bc51c2dabd4b302c3d951a4e578081
sfia-o <sofia.vl.oliveira@gmail.com>
2023-06-07 00:24:31 +0000

    Incorporated mocha and chai into test file

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,31 @@
 const eqArrays = require('../eqArrays');
 const assertArraysEqual = require ('../assertArraysEqual');
+
+const assert = require('chai').assert;
 const middle = require ('../middle');
 
+describe('#middle', () => {
 
-console.log(middle([1, 2]));
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6]));
-console.log(middle([1, 3, 4]));
+  it('returns the middle element when passed an odd array', () => {
+    assert.deepEqual(middle([1,2,3]), [2])
+  });
+
+  it('returns the two middle elements when passed an even array', () => {
+    assert.deepEqual(middle([1,2,3,4]), [2,3])
+  });
+
+  it('returns an empty array when passed a single element array', () => {
+    assert.deepEqual(middle([1]), []);
+  });
+
+  it('returns an empty array when passed an empty array', () => {
+    assert.deepEqual(middle([]), []);
+  });
+
+
+});
+
+// console.log(middle([1, 2]));
+// console.log(middle([1, 2, 3, 4])); // => [2, 3]
+// console.log(middle([1, 2, 3, 4, 5, 6]));
+// console.log(middle([1, 3, 4]));
