bc7c4d346cc58447f22013cf990d451c72e531bd
sfia-o <sofia.vl.oliveira@gmail.com>
2023-06-07 17:18:22 +0000

    Reversed require refactoring

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,4 +1,16 @@
-const eqArrays = require('./eqArrays')
+const eqArrays = function(arr1, arr2){
+
+  if (arr1.length !== arr2.length){
+    return false;
+  };
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]){
+      return false;
+    }
+   } 
+   return true;
+  };
 
 const assertArraysEqual = function(arr3, arr4) {
   if (eqArrays(arr3, arr4)){
--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,6 +1,40 @@
-const eqArrays = require('./eqArrays');
+const eqArrays = function(arr1, arr2){
 
-const eqObjects = require('./eqObjects');
+  if (arr1.length !== arr2.length){
+    return false;
+  };
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]){
+      return false;
+    }
+   } 
+   return true;
+  };
+
+const eqObjects = function(object1, object2) {
+
+  const obj1 = Object.keys(object1);
+  const obj2 = Object.keys(object2);
+ 
+  
+  if (obj1.length !== obj2.length) {
+    return false;
+  }
+  for (const key of obj1) {
+
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
+    } else if (object1[key] !== object2[key]) {
+      return false;
+    }
+
+  } return true;
+
+};
 
 const assertObjectsEqual = function(actual, expected) {
 
